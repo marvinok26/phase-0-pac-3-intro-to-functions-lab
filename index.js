a724ab8daa381ae595ad974d37dece4cfde29e4a
Marvin <okongomarvin971@gmail.com>
2023-12-10 09:55:12 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ function logWhisper(string) {
 logWhisper("HELLO")
 
 function sayHiToHeadphonedRoommate(string){
-    if (string == string.toLowerCase()){
+    if (string === string.toLowerCase()){
         return "I can't hear you!";
     }
-    else if (string == string.toUpperCase()){
+    else if (string === string.toUpperCase()){
         return "YES INDEED!";
     }
-    else if (string == "Let\'s have dinner together!"){
+    else if (string === "Let\'s have dinner together!"){
         return "I would love to!";
     }
     else{
